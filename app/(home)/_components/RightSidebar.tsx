db823e33dd447f01ac353ce4e39263030e2e1b12
jiminsample1 <jiminsample1@gmail.com>
2024-07-31 23:47:10 +0700

    update

--- app/(home)/_components/RightSidebar.tsx
+++ app/(home)/_components/RightSidebar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <div className="sticky top-[90px] p-[20px]">
+      <div className="block  mb-[12px] text-[20px] font-semibold uppercase text-gray-500">
+        Phổ biến
+      </div>
+      <div className="w-full h-[200px] bg-gray-200"></div>
+      <div className="mt-4 w-full h-[200px] bg-gray-200"></div>
+      <div className="mt-4 w-full h-[200px] bg-gray-200"></div>
+    </div>
+  );
 };
-
 export default RightSidebar;
--- data/static.ts
+++ data/static.ts
@@ -1,6 +1,10 @@
 import { PiSuitcaseSimpleFill, PiCoffeeFill } from "react-icons/pi";
 import { FaLightbulb } from "react-icons/fa";
 import { FaBellConcierge } from "react-icons/fa6";
+import { FaDatabase } from "react-icons/fa";
+import { GrSystem } from "react-icons/gr";
+import { CgBrowser } from "react-icons/cg";
+import { FaNode } from "react-icons/fa";
 export const LEFT_SIDE_BAR = [
   {
     text: "Kĩ năng mềm",
@@ -21,5 +25,28 @@ export const LEFT_SIDE_BAR = [
     text: "Thưởng thức",
     href: "/",
     icon: FaBellConcierge,
+  },
+];
+
+export const SERIES = [
+  {
+    text: "Front end",
+    href: "/front-end",
+    icon: CgBrowser,
+  },
+  {
+    text: "Back end",
+    href: "/back-end",
+    icon: FaNode,
+  },
+  {
+    text: "CI/CD",
+    href: "/ci-cd",
+    icon: GrSystem,
+  },
+  {
+    text: "Database",
+    href: "/database",
+    icon: FaDatabase,
   },
 ];
