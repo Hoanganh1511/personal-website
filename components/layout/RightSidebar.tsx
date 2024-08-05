6c5160e58c4a2dbc222a81502190d07f0f6dfb05
jiminsample1 <jiminsample1@gmail.com>
2024-08-05 12:56:50 +0700

    update

--- components/layout/RightSidebar.tsx
+++ components/layout/RightSidebar.tsx
@@ -5,7 +5,7 @@ const RightSidebar = () => {
   return (
     <div className="sticky top-[90px] p-[20px]">
       <div>
-        <div className="block  mb-[12px] text-[20px] font-semibold uppercase text-gray-500">
+        <div className="block mb-[12px] text-[20px] font-semibold uppercase text-gray-500">
           Phổ biến
         </div>
         <div className="w-full h-[400px] bg-gray-200"></div>
